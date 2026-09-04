ab3edd6185d0606fa943fa050ec8eddfe326475c
Ryan-Kao <112361057+Ryan-Kao@users.noreply.github.com>
2026-09-03 18:07:04 -0700

    Added stages, made changes to coin gained and added new variables to adjust.

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ let autoClickerPower = 0;
 let autoClickerCost = 100;
 let upgradeCost2 = 300;
 let clickPower2 = 0;
-let coins = 0;
+let currentCoins = 0;
+let coinsGained = 100;
+let stage = 1;
 
 const button = document.querySelector("#btn");
 const upgrade = document.querySelector("#upgrade");
@@ -19,46 +21,50 @@ const upgradeDisplay2 = document.querySelector("#upgradeCost2");
 const clickPowerDisplay = document.querySelector("#currentClickPower");
 const autoClickerDisplay = document.querySelector("#currentAutoClickers");
 const coinsDisplay = document.querySelector("#currentCoins");
+const newStageDisplay = document.querySelector("#currentStage");
 
 button.addEventListener("click", () => {
     monsterHP -= clickPower;
     monsterHP -= clickPower2;
 
     if (monsterHP <= 0) {
-        coins += 100;
+        stage++;
+        currentCoins += Math.round(coinsGained);
+        coinsGained *= 1.3;
         monsterMaxHP *= 1.2;
         monsterHP = Math.round(monsterMaxHP);
     }
 
-    coinsDisplay.textContent = coins;
+    newStageDisplay.textContent = stage;
+    coinsDisplay.textContent = currentCoins;
     monsterHPDisplay.textContent = monsterHP;
 });
 
 
 upgrade.addEventListener("click", () => {
-    if (coins >= upgradeCost) {
-        coins -= upgradeCost;
+    if (currentCoins >= upgradeCost) {
+        currentCoins -= upgradeCost;
         clickPower++;
 
-        upgradeCost *= 1.2;
+        upgradeCost *= 1.4;
         upgradeCost = Math.round(upgradeCost);
     }
 
-    coinsDisplay.textContent = coins;
+    coinsDisplay.textContent = currentCoins;
     upgradeDisplay.textContent = upgradeCost;
     clickPowerDisplay.textContent = clickPower + clickPower2;
 });
 
 autoClickerBtn.addEventListener("click", () => {
-    if (coins >= autoClickerCost) {
-        coins -= autoClickerCost;
+    if (currentCoins >= autoClickerCost) {
+        currentCoins -= autoClickerCost;
         autoClickerPower++;
 
-        autoClickerCost *= 1.2;
+        autoClickerCost *= 1.4;
         autoClickerCost = Math.round(autoClickerCost);
     }
 
-    coinsDisplay.textContent = coins;
+    coinsDisplay.textContent = currentCoins;
     autoClickerCostDisplay.textContent = autoClickerCost;
     autoClickerDisplay.textContent = autoClickerPower;
 });
@@ -66,27 +72,28 @@ autoClickerBtn.addEventListener("click", () => {
 function autoClicker() {
     monsterHP -= autoClickerPower;
     if (monsterHP <= 0) {
-        coins += 100;
+        currentCoins += Math.round(coinsGained);
+        coinsGained *= 1.3;
         monsterMaxHP *= 1.2;
         monsterHP = Math.round(monsterMaxHP);
     }
 
-    coinsDisplay.textContent = coins;
+    coinsDisplay.textContent = currentCoins;
     monsterHPDisplay.textContent = monsterHP;
 }
 
 setInterval(autoClicker, 2000);
 
 upgrade2.addEventListener("click", () => {
-    if (coins >= upgradeCost2) {
-        coins -= upgradeCost2
+    if (currentCoins >= upgradeCost2) {
+        currentCoins -= upgradeCost2
         clickPower2 += 20
 
         upgradeCost2 *= 1.4;
         upgradeCost2 = Math.round(upgradeCost2);
     }
 
-    coinsDisplay.textContent = coins;
+    coinsDisplay.textContent = currentCoins;
     upgradeDisplay2.textContent = upgradeCost2;
     clickPowerDisplay.textContent = clickPower + clickPower2;
 });
